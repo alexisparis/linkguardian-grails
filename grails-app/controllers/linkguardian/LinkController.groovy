317feb9bebfa2bebfe02f3c82fe526c87fdfe2f5
Alexis PARIS <alexis.paris@blackdog-project.org>
2013-01-13 20:38:17 +0100

    first commit

---
+++ grails-app/controllers/linkguardian/LinkController.groovy
@@ -0,0 +1,11 @@
+package linkguardian
+
+class LinkController
+{
+
+    def index()
+    {
+      println("calling index from LinkController")
+      //Link.get
+    }
+}
---
+++ grails-app/domain/linkguardian/Link.groovy
@@ -0,0 +1,12 @@
+package linkguardian
+
+class Link {
+
+    static constraints = {
+    }
+
+  String url
+  Note note
+  Date creationDate
+  String[] tags
+}
