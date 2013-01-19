f8983c123fd60267bfcf09641c273d816be32150
Alexis PARIS <alexis.paris@blackdog-project.org>
2013-01-19 21:51:59 +0100

    first try to include filters and add link form

---
+++ grails-app/domain/linkguardian/Note.groovy
@@ -0,0 +1,12 @@
+package linkguardian
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexis
+ * Date: 13/01/13
+ * Time: 20:30
+ * To change this template use File | Settings | File Templates.
+ */
+enum Note
+{
+  Note_1, Note_2, Note_3, Note_4, Note_5
+}
