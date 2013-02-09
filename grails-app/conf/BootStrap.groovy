af2376c3318cc0bae3d7685562cde154b46e6d27
Alexis PARIS <alexis.paris@blackdog-project.org>
2013-02-09 23:45:08 +0100

    fix the database production url

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,18 +8,40 @@ class BootStrap {
 
   def linkBuilderService
 
+    def createLink(String _url, String _tags, Note _note, Boolean _read = false)
+    {
+        try {
+            def newLink = new Link(url: _url, fusionedTags: _tags, creationDate: new Date(), read:  _read)
+            newLink.note = _note
+            linkBuilderService.complete(newLink)
+            newLink.save(flush: true)
+        }
+        catch(Exception e)
+        {
+            log.error("error while creating link with url '" + _url + "'", e)
+        }
+    }
+
     def init    = { servletContext ->
         def newLink
 
-        newLink = new Link(url: "http://www.privatesportshop.com", fusionedTags: " sport shop nike adidas reebok a ", creationDate: new Date())
-        newLink.note = Note.Note_2
-        linkBuilderService.complete(newLink)
-        newLink.save(flush: true)
+        this.createLink("http://www.privatesportshop.com", " sport shop nike adidas reebok a ", Note.Note_2)
 
-        newLink = new Link(url: "http://www.brandalley.fr", fusionedTags: " shop marque ", creationDate: new Date())
-        newLink.note = Note.Note_4
-        linkBuilderService.complete(newLink)
-        newLink.save(flush: true)
+        this.createLink("http://www.brandalley.fr", " shop marque ", Note.Note_4)
+
+        this.createLink("http://blog.knoldus.com/2013/01/12/akka-futures-in-scala-with-a-simple-example/", " scala akka ", Note.Note_0, true)
+
+        this.createLink("http://korben.info/idees-raspberry-pi.html", " scala akka ", Note.Note_0)
+
+        this.createLink("http://use-the-index-luke.com/sql/preface", " sql ", Note.Note_0)
+
+        this.createLink("http://www.house-fr.com/dossiers/Les_baskets_de_House", " basket docteur house ", Note.Note_0, true)
+
+        this.createLink("http://arodrigues.developpez.com/tutoriels/java/presentation-apache-jmeter-partie1/", " jmeter ", Note.Note_0, true)
+
+        this.createLink("http://aravindamadusanka.blogspot.fr/2012/08/how-to-use-apache-jmeter-for-web.html", " jmeter ", Note.Note_0)
+
+        this.createLink("http://java.dzone.com/articles/introducing-spring-integration", " spring integration ", Note.Note_0)
 
         //newLink = new Link(url: "http://www.laredoute.fr", fusionedTags: " ", creationDate: new Date())
         //newLink.note = Note.Note_5
@@ -35,41 +57,6 @@ class BootStrap {
         //newLink.note = Note.Note_0
         //linkBuilderService.complete(newLink)
         //newLink.save(flush: true)
-
-        newLink = new Link(url: "http://blog.knoldus.com/2013/01/12/akka-futures-in-scala-with-a-simple-example/", fusionedTags: " scala akka ", creationDate: new Date(), read: true)
-        newLink.note = Note.Note_0
-        linkBuilderService.complete(newLink)
-        newLink.save(flush: true)
-
-        newLink = new Link(url: "http://korben.info/idees-raspberry-pi.html", fusionedTags: " scala akka ", creationDate: new Date())
-        newLink.note = Note.Note_0
-        linkBuilderService.complete(newLink)
-        newLink.save(flush: true)
-
-        newLink = new Link(url: "http://use-the-index-luke.com/sql/preface", fusionedTags: " sql ", creationDate: new Date())
-        newLink.note = Note.Note_0
-        linkBuilderService.complete(newLink)
-        newLink.save(flush: true)
-
-        newLink = new Link(url: "http://www.house-fr.com/dossiers/Les_baskets_de_House", fusionedTags: " basket docteur house ", creationDate: new Date(), read: true)
-        newLink.note = Note.Note_0
-        linkBuilderService.complete(newLink)
-        newLink.save(flush: true)
-
-        newLink = new Link(url: "http://arodrigues.developpez.com/tutoriels/java/presentation-apache-jmeter-partie1/", fusionedTags: " jmeter ", creationDate: new Date(), read: true)
-        newLink.note = Note.Note_0
-        linkBuilderService.complete(newLink)
-        newLink.save(flush: true)
-
-        newLink = new Link(url: "http://aravindamadusanka.blogspot.fr/2012/08/how-to-use-apache-jmeter-for-web.html", fusionedTags: " jmeter ", creationDate: new Date())
-        newLink.note = Note.Note_0
-        linkBuilderService.complete(newLink)
-        newLink.save(flush: true)
-
-        newLink = new Link(url: "http://java.dzone.com/articles/introducing-spring-integration", fusionedTags: " spring integration ", creationDate: new Date())
-        newLink.note = Note.Note_0
-        linkBuilderService.complete(newLink)
-        newLink.save(flush: true)
 
         def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
         def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
