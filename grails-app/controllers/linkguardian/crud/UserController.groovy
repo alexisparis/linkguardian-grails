a7a1a25ebd97572f0ce9c75505056f037f1aa24f
Alexis PARIS <alexis.paris@blackdog-project.org>
2013-02-10 15:12:48 +0100

    fix the database production url

--- grails-app/controllers/linkguardian/crud/UserController.groovy
+++ grails-app/controllers/linkguardian/crud/UserController.groovy
@@ -3,7 +3,7 @@ package linkguardian.crud
 import grails.plugins.springsecurity.Secured
 import linkguardian.Person
 
-@Secured(['ROLE_ADMIN'])
+//@Secured(['ROLE_ADMIN'])
 class UserController
 {
     static scaffold = Person
